03224a5254c04bce2f71f20bbc671ab34ef86059
Andrew Ruzankin <rtzdota@gmail.com>
2023-02-15 09:19:24 +0700

    just starting

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        int rows = 35;
-        for (int i = 1; i <= rows; i++)
-        {
-            for (int j = 0; j < i; j++)
-            {
-                System.out.print("# ");
-            }
-            System.out.println();
-            //test for git1
-        }
-        }
+        Scanner input = new Scanner(System.in);
+        int i1 = input.nextInt();
+        double d1 = input.nextDouble();
+        float f1 = input.nextFloat();
+        Boolean b1 = input.nextBoolean();
+        System.out.println(i1 + " " + d1 + " " +f1 + " " + b1);
+        double res = i1 - d1;
+        System.out.println(res);
+    }
     }
